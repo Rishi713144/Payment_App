f0171de9bb0fb266989f31733b3534967b62c3a1
Rishi <116449371+Rishi713144@users.noreply.github.com>
2025-11-20 22:59:08 +0530

    Update index.js

--- PayTm/backend/api/index.js
+++ PayTm/backend/api/index.js
@@ -4,16 +4,12 @@ const rootRouter = require("../routes/index");
 
 const app = express();
 
-const cors = require("cors");
-
 app.use(cors({
-  origin: ["https://payment-app-delta-two.vercel.app"]
+    origin:["https://payment-app-delta-two.vercel.app/"]
 }));
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
 
-app.get("/", (req, res) => {
-  res.json({ message: "API is running! Use /api/v1/..." });
-});
+
 module.exports = app;
